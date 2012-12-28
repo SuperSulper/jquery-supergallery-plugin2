f7e7e06f92b2ff74e4a3ab4d3e00f9edac703c9d
otto <otto@mgzl.jp>
2012-12-29 00:36:58 +0900

    fix

--- grunt.js
+++ grunt.js
@@ -52,5 +52,5 @@ module.exports = function(grunt){
 			}
 		}
 	});
-	grunt.registerTask('default', 'concat less');
+	grunt.registerTask('default', 'concat min');
 };
--- src/core.js
+++ src/core.js
@@ -1,7 +1,7 @@
 
-var Supergallery = function(target,_options){
-	_options = _options || {};
-	this.options = {
+var Supergallery = function(target,_o){
+	_o = _o || {};
+	this.o = {
 		selectors:{
 			main:'#main',
 			thumb:'#thumb',
@@ -24,14 +24,15 @@ var Supergallery = function(target,_options){
 			loop:true
 		}
 	};
-	$.extend(true,this.options,_options);
+	$.extend(true,this.o,_o);
+
 	this.$target = $(target);
-	this.$main = this.$target.find(this.options.selectors.main);
+	this.$main = this.$target.find(this.o.selectors.main);
 	this.$mainChildren = this.$main.children();
-	this.$thumb = this.$target.find(this.options.selectors.thumb);
+	this.$thumb = this.$target.find(this.o.selectors.thumb);
 	this.$thumbChildren = this.$thumb.children();
-	this.$nextBtn = $(this.options.selectors.nextBtn);
-	this.$prevBtn = $(this.options.selectors.prevBtn);
+	this.$nextBtn = this.$target.find(this.o.selectors.nextBtn);
+	this.$prevBtn = this.$target.find(this.o.selectors.prevBtn);
 	this.current = null;
 	this.timerId = null;
 	this.num = this.$mainChildren.length;
@@ -41,32 +42,32 @@ var Supergallery = function(target,_options){
 Supergallery.prototype.init = function(){
 	var sg = this,eventType = ('ontouchend' in window) ? 'touchend' : 'click';
 
-	if(!this.$main.length){
-		throw 'mainの数が0個です。セレクタが間違っているかもしれません。 this.options.selectors.main : ' + this.options.selectors.main;
+	if(!sg.$main.length){
+		throw 'mainの数が0個です。セレクタが間違っているかもしれません。 this.o.selectors.main : ' + sg.o.selectors.main;
 	}
 	if(!this.$mainChildren.length){
 		throw 'mainの中に何もありません！';
 	}
 
 
-	this.$main
+	sg.$main
 		.css({
 			position:'relative',
 			overflow:'hidden'
 		});
-	this.$mainChildren
+	sg.$mainChildren
 		.css({
 			position:'absolute'
 		})
-		.eq(this.options.other.initialSelect)
+		.eq(sg.o.other.initialSelect)
 			.css({display:'block'})
 		.end()
-		.not(':eq(' +this.options.other.initialSelect+ ')')
+		.not(':eq(' +sg.o.other.initialSelect+ ')')
 			.css({display:'none'});
 
-	this.changeTo(this.options.other.initialSelect,true);
-	if(this.$thumbChildren.length){	
-		this.$thumbChildren
+	sg.changeTo(sg.o.other.initialSelect,true);
+	if(sg.$thumbChildren.length){	
+		sg.$thumbChildren
 			.each(function(n){
 				$(this)
 					.on(eventType,function(){
@@ -74,36 +75,36 @@ Supergallery.prototype.init = function(){
 					});
 			});
 	}
-	if(this.$nextBtn.length){
-		this.$nextBtn.on(eventType,function(){
+	if(sg.$nextBtn.length){
+		sg.$nextBtn.on(eventType,function(){
 			var target = sg.current + 1;
 			if(target < sg.num){
 				sg.changeTo(target);
 			}else{
-				if(sg.options.other.loop){
+				if(sg.o.other.loop){
 					sg.changeTo(0);
 				}
 			}
 		});
 	}
-	if(this.$prevBtn.length){
-		this.$prevBtn.on(eventType,function(){
+	if(sg.$prevBtn.length){
+		sg.$prevBtn.on(eventType,function(){
 			var target = sg.current - 1;
 			if(target >= 0){
 				sg.changeTo(target);
 			}else{
-				if(sg.options.other.loop){
+				if(sg.o.other.loop){
 					sg.changeTo(sg.num - 1);
 				}
 			}
 		});
 	}
 
-	if(this.options.timer.enable && this.options.timer.interval){
-		this.setTimer();
+	if(sg.o.timer.enable && sg.o.timer.interval){
+		sg.setTimer();
 	}
-	if(this.options.timer.enable && this.options.timer.interval && this.options.timer.stopOnHover){
-		this.$target
+	if(sg.o.timer.enable && sg.o.timer.interval && sg.o.timer.stopOnHover){
+		sg.$target
 			.hover(function(){
 				sg.clearTimer();
 			},function(){
@@ -114,13 +115,13 @@ Supergallery.prototype.init = function(){
 };
 Supergallery.prototype.changeTo = function(n,noAnimation){
 	var sg = this;
-	if(n === this.current){ return false;}
-	this.$target.trigger('pageChangeStart',n);
-	var duration = noAnimation ? 0 :this.options.animation.duration;
-	var oldNum = this.current;
-	var $_target = this.$mainChildren.eq(n),$_oldTarget = this.$mainChildren.eq(oldNum);
+	if(n === sg.current){ return false;}
+	sg.$target.trigger('pageChangeStart',n);
+	var duration = noAnimation ? 0 :sg.o.animation.duration;
+	var oldNum = sg.current;
+	var $_target = sg.$mainChildren.eq(n),$_oldTarget = sg.$mainChildren.eq(oldNum);
 
-	if(this.options.animation.type === 'fade'){
+	if(sg.o.animation.type === 'fade'){
 		$_target
 			.stop(true,false)
 			.fadeTo(duration,1);
@@ -157,12 +158,12 @@ Supergallery.prototype.changeTo = function(n,noAnimation){
 			.stop(true,false)
 			.animate({
 				left:0
-			},duration,sg.options.animation.easing);
+			},duration,sg.o.animation.easing);
 			$_oldTarget
 				.stop(true,false)
 				.animate({
 					left:endPos
-				},duration,sg.options.animation.easing,function(){
+				},duration,sg.o.animation.easing,function(){
 					$_oldTarget
 						.css({
 							display:'none'
@@ -172,40 +173,41 @@ Supergallery.prototype.changeTo = function(n,noAnimation){
 		}
 	}
 
-	this.$thumbChildren
+	sg.$thumbChildren
 		.eq(n)
-			.addClass(this.options.other.selectedClassName)
+			.addClass(sg.o.other.selectedClassName)
 		.end()
 		.not(':eq(' + n + ')')
-			.removeClass(this.options.other.selectedClassName);
-	this.current = n;
+			.removeClass(sg.o.other.selectedClassName);
+	sg.current = n;
 };
 
 Supergallery.prototype.setTimer = function(){
 	var sg = this;
-	if(this.timerId){
-		this.clearTimer();
+	if(sg.timerId){
+		sg.clearTimer();
 	}
-	this.timerId = setInterval(function(){
+	sg.timerId = setInterval(function(){
 		var target = sg.current + 1;
 		if(target < sg.num){
 			sg.changeTo(target);
 		}else{
-			if(sg.options.other.loop){
+			if(sg.o.other.loop){
 				sg.changeTo(0);
 			}else{
 				sg.clearTimer();
 			}
 		}
-	},this.options.timer.interval);
+	},sg.o.timer.interval);
 };
 
 Supergallery.prototype.clearTimer = function(){
 	clearInterval(this.timerId);
 };
-var core = function(targetSelector,_options){
-	return new Supergallery(targetSelector,_options);
-}
-var fn = function(_options){
-	return new Supergallery(this,_options);
+var core = function(targetSelector,_o){
+	return new Supergallery(targetSelector,_o);
+};
+var fn = function(_o){
+	new Supergallery(this,_o);
+	return this;
 };
