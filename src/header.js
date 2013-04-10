3a1fc816301b2faf91a7561b26b811608af81c2d
otto <otto@mgzl.jp>
2013-04-10 16:42:30 +0900

    ページインジケーター機能追加・readme修正

--- src/header.js
+++ src/header.js
@@ -1,6 +1,6 @@
-/*! jQuery Supergallery Plugin2 2012-12-28
-* Vertion: 1.0
-* Depends: jQuery 1.8.x
+/*! jQuery Supergallery Plugin2 2013-4-10
+* Vertion: 1.2
+* Depends: jQuery 1.8.0 - 1.9.1
 * Author: Otto Kamiya (MegazalRock) http://mgzl.jp/
 * License: MIT License*/
 (function($){
